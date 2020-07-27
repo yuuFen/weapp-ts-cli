e18b3c6b094c2e01afcccf78cf7cbf4e1bc7dc57
Yu <me@yuufen.com>
2020-07-28 07:20:37 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 10) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'pmCli requires Node 10 or higher. \n' +
+      'Please update your version of Node.'
+  );
+  process.exit(1);
+}
+
+require('../packages/initialization')();
