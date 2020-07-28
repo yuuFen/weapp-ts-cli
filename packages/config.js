2599475d9304adc3cad00f7f73efeafb9cb646dc
Yu <me@yuufen.com>
2020-07-29 01:28:01 +0800

    config

---
+++ packages/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // 根目录
+  root: __dirname,
+  // 执行命令目录路径
+  dir_root: process.cwd(),
+  // 小程序项目路径
+  entry: './',
+  // 项目编译输出文件夹
+  output: './',
+  // 小程序模版目录
+  template: __dirname + '/template',
+  project_temp_repo: 'yuuFen/weapp-ts-template',
+};
